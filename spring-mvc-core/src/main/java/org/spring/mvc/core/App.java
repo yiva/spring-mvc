dba6a6ab3fcd0f13b9a5c951cb80caa08b3effeb
yiva <wyhtsh@gmail.com>
2016-11-29 16:30:01 +0800

    create spring mvc project

---
+++ spring-mvc-core/src/main/java/org/spring/mvc/core/App.java
@@ -0,0 +1,13 @@
+package org.spring.mvc.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
